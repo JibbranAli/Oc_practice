c0b61302b1d1f9dff9ce2d8d55f8ddb318da5a1e
Syed Jibbran Ali <90756497+JibbranAli@users.noreply.github.com>
2023-01-25 14:04:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <body>
 
 <h1>Question one</h1>
-
-<?php echo "Successfully completed question one "; ?>
+<?php echo "Successfully Added the feature in the app  "; ?>
+<?php echo "Successfully completed question TWo "; ?>
 
 </body>
 </html> 
